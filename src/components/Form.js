dfd2c1ac8c7da48587394e12c12a9b9c29022c9c
unknown <sergio-caballero_student2021@wilder.school>
2021-04-11 14:53:53 +0200

    useState of meetings added

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Form=()=>{
+    const [meeting, setMeeting]= useState({
+        pet:"",
+        owner:"",
+        date:"",
+        time:"",
+        symptoms:""
+    });
+    const handlerSetMeeting=(e)=>{
+        setMeeting({
+            ...meeting,
+            [e.target.name]:[e.target.value]
+
+        })
+    }
+    const{owner, date, time,symptoms, pet} = meeting
     return (
         <>
             <h2> Create a meeting</h2>
@@ -11,6 +26,8 @@ const Form=()=>{
                 name="pet"
                 className="u-full-width"
                 placeholder="Pet name"
+                onChange={handlerSetMeeting}
+                value={pet}
                 />
                 <label>Owner's Name</label>
                 <input
@@ -18,12 +35,16 @@ const Form=()=>{
                 name="owner"
                 className="u-full-width"
                 placeholder="Owner's Name"
+                onChange={handlerSetMeeting}
+                value={owner}
                 />
                 <label>Date</label>
                 <input
                 type="date"
                 name="date"
                 className="u-full-width"
+                onChange={handlerSetMeeting}
+                value={date}
                 
                 />
                 <label>Time</label>
@@ -31,11 +52,15 @@ const Form=()=>{
                 type="time"
                 name="time"
                 className="u-full-width"
+                onChange={handlerSetMeeting}
+                value={time}
                 />
                 <label>symptoms</label>
                 <textarea 
                 name="symptoms"
                 className="u-full-width"
+                onChange={handlerSetMeeting}
+                value={symptoms}
                 ></textarea>
                 <button
                     type="submit"
